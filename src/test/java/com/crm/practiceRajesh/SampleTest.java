7333569171b32ad0cac7099efc3348a05c89c673
TestScriptGitBash <rajeshbhukya473@gmail.com>
2022-05-10 16:10:23 +0530

    send

---
+++ src/test/java/com/crm/practiceRajesh/SampleTest.java
@@ -0,0 +1,13 @@
+package com.crm.practiceRajesh;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void RajTest() {
+		System.out.println("sendRequest");
+	}
+	
+	
+
+}
